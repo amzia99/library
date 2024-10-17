ed30bb61da171e3721de99c923dedf3fafd5cc5b
Abdullah Zia <amz1cflp99@gmail.com>
2024-10-17 19:24:06 -0400

    implemented the print tracks function

--- library.js
+++ library.js
@@ -37,14 +37,16 @@ const printPlaylists = function() {
        }
 };
 
-
 // prints a list of all tracks, using the following format:
 // t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
 // t02: Model View Controller by James Dempsey (WWDC 2003)
 // t03: Four Thirty-Three by John Cage (Woodstock 1952)
 const printTracks = function() {
-
-}
+       for (const trackId in library.tracks) {
+              const track = library.tracks[trackId];
+              console.log(`${track.id}: ${track.name} by ${track.artist} (${track.album})`);
+       }
+};
 
 
 // prints a list of tracks for a given playlist, using the following format:
